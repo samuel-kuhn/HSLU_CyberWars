1bfa2fdd68d00fc3bf400546886e047a4a6e3a6a
Samuel Kuhn <samuel.kuhn@gmx.ch>
2025-10-18 19:24:48 +0200

    generate zip folder on the fly and put it into home folder

--- scripts/create-users.sh
+++ scripts/create-users.sh
@@ -14,3 +14,7 @@ cp -r "backend-api/Management API/Docker Compose" /home/$USERNAME/backend-api
 chown -R $USERNAME:$USERNAME /home/$USERNAME/backend-api
 echo "$PASSWORD" > /home/$USERNAME/backend-api/auth_password.txt
 
+cd "backend-api/Management API/Docker"
+zip -r --password " pussycatdolls" /home/$USERNAME/source.zip ./*
+cd - > /dev/null 
+chown $USERNAME:$USERNAME /home/$USERNAME/source.zip
