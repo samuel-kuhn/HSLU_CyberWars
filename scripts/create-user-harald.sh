cac018e278be9fd62b88ff34df385c05d53cd241
Samuel Kuhn <samuel.kuhn@gmx.ch>
2025-10-18 17:45:23 +0200

    updated create-user script

--- scripts/create-user-harald.sh
+++ scripts/create-user-harald.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+USERNAME="harald"
+PASSWORD="TODO updatethispassword"
+
+# creating user
+useradd -m $USERNAME
+
+#changing password
+echo "$USERNAME:$PASSWORD" | chpasswd
+
+# copying docker files to home folder
+cp -r "backend-api/Management API/Docker Compose" /home/$USERNAME/backend-api
+chown -R $USERNAME:$USERNAME /home/$USERNAME/backend-api
+echo "$PASSWORD" > /home/$USERNAME/backend-api/auth_password.txt
+
--- setup-script.sh
+++ setup-script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+scripts/create-user-harald.sh
