b80ebec952ab8fbc9e6b99262cad6e324bfe1009
Cod3d1PA <142023715+Cod3d1PA@users.noreply.github.com>
2025-10-19 11:33:08 +0200

    Add Nginx installation and configuration steps. (Phase 1)
    Install Nginx and configure for website deployment.

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -18,6 +18,18 @@ apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
+### Website (Phase 1) ###
+apt-get update
+apt-get install nginx -y
+
+sudo mv "${GIT_LOCATION}"/website/zhhk /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/zhhk /etc/nginx/sites-enabled/
+
+sudo cp -r "${GIT_LOCATION}"/website/* /var/www/html
+sudo rm /var/www/html/README.md
+
+sudo systemctl restart nginx
+ ### Phase 1 complete ###
 
 #### Other tools ####
 
