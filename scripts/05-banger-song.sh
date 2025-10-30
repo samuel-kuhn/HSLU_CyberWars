156ac6d56f22c4c3a012c06c221766250fff23af
Daemu77 <damianboesch20@gmail.com>
2025-10-30 15:20:04 -0400

    Flag zum Script hinzugefügt

--- scripts/05-banger-song.sh
+++ scripts/05-banger-song.sh
@@ -6,3 +6,6 @@ chown -R haraldl:haraldl /home/haraldl/notizen.txt
 chown -R haraldl:haraldl /home/haraldl/zhhk.mp3
 chmod 770 /home/haraldl/notizen.txt
 chmod 770 /home/haraldl/zhhk.mp3
+
+touch /root/.flag.txt
+echo "flag{D4s_si3ht_ab3r_g4r_n1cht_guT_au$}" > /root/.flag.txt
