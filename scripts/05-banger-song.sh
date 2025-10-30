696d5a056e1b774cc00f54b2cf6127aa5b912641
Daemu77 <damianboesch20@gmail.com>
2025-10-30 15:02:01 -0400

    Unix Konvertiertes notizen file upload

--- scripts/05-banger-song.sh
+++ scripts/05-banger-song.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 mv Song/notizen.txt /home/haraldl/notizen.txt
 mv Song/zhhk.mp3 /home/haraldl/zhhk.mp3
 
-chown haraldl:haraldl /home/haraldl/notizen.txt
-chown haraldl:haraldl /home/haraldl/zhhk.mp3
+chown -R haraldl:haraldl /home/haraldl/notizen.txt
+chown -R haraldl:haraldl /home/haraldl/zhhk.mp3
 chmod 770 /home/haraldl/notizen.txt
 chmod 770 /home/haraldl/zhhk.mp3
-
